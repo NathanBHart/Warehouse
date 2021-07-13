66e31700c605e510219d7563f4d30e2ba37f5789
Brady <thebradster7.bd@gmail.com>
2021-07-12 21:50:00 -0400

    No longer says "hi".

--- World/Door.gd
+++ World/Door.gd
@@ -6,7 +6,6 @@ export(String, FILE, "*.tscn") var NEW_LEVEL_PATH = ""
 var is_active = true
 
 func _on_Door_body_entered(player):
-	print("hi")
 	if is_active:
 		player.emit_signal("hit_door", self)
 		is_active = false
