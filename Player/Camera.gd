3d240bfcb8f4794ff48c554792b302ab89972b54
Brady <thebradster7.bd@gmail.com>
2021-07-12 21:41:00 -0400

    Refactored set_limits in camera.

--- Player/Camera.gd
+++ Player/Camera.gd
@@ -1,8 +1,5 @@
 extends Camera2D
 
-onready var topLeftPosition = null
-onready var bottomRightPosition = null
-
 var MainInstances = ResourceLoader.MainInstances
 
 func _ready():
@@ -11,18 +8,10 @@ func _ready():
 func _process(_delta):
 	player_at_edge_check()
 
-func set_limits():
-	if topLeftPosition != null:
-		limit_left = topLeftPosition.x
-		limit_top = topLeftPosition.y
-		
-	if bottomRightPosition != null:
-		limit_right = bottomRightPosition.x
-		limit_bottom = bottomRightPosition.y
-
 func player_at_edge_check():
-	if MainInstances.Player != null:
-		if MainInstances.Player.global_position.x < limit_left:
-			MainInstances.Player.global_position.x = limit_left
-		elif MainInstances.Player.global_position.x > limit_right:
-			MainInstances.Player.global_position.x = limit_right
+	var Player = MainInstances.Player
+	if Player != null:
+		if Player.global_position.x < limit_left:
+			Player.global_position.x = limit_left
+		elif Player.global_position.x > limit_right:
+			Player.global_position.x = limit_right
--- Rooms/Room.gd
+++ Rooms/Room.gd
@@ -14,6 +14,9 @@ func _ready():
 		parent.currentRoom = self
 
 func set_camera_limits():
-	MainInstances.MainCamera.topLeftPosition = topLeft.global_position
-	MainInstances.MainCamera.bottomRightPosition = bottomRight.global_position
-	MainInstances.MainCamera.set_limits()
+	var MainCamera = MainInstances.MainCamera
+	if MainCamera != null:
+		MainCamera.limit_top = topLeft.global_position.y
+		MainCamera.limit_left = topLeft.global_position.x
+		MainCamera.limit_bottom = bottomRight.global_position.y
+		MainCamera.limit_right = bottomRight.global_position.x
