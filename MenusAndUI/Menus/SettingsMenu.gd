e981aff9db16c1d54ad84865aded5085c2c1e45b
Brady <thebradster7.bd@gmail.com>
2021-07-15 19:12:42 -0400

    Added pause menu.

--- MenusAndUI/Menus/SettingsMenu.gd
+++ MenusAndUI/Menus/SettingsMenu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+export var IN_MAIN = false
+
+var pauseMenu = null
+
+onready var colorRect = $ColorRect
+
 # Sliders
 onready var brightnessSlider = $MarginContainer/TabContainer/Video/MarginContainer2/HBoxContainer/VBoxContainer/BrightnessSlider
 onready var soundEffectsSlider = $MarginContainer/TabContainer/Audio/MarginContainer/HBoxContainer/VBoxContainer/SoundEffectsSlider
@@ -7,6 +13,8 @@ onready var musicSlider = $MarginContainer/TabContainer/Audio/MarginContainer/HB
 onready var difficultySlider = $MarginContainer/TabContainer/Gameplay/MarginContainer/HBoxContainer/VBoxContainer/DifficultySlider
 
 func _ready():
+	if IN_MAIN:
+		colorRect.color = Color(0, 0, 0, 0.403922)
 	call_deferred("set_settings")
 
 func _process(_delta):
@@ -14,6 +22,12 @@ func _process(_delta):
 	Settings.sound_effects = soundEffectsSlider.value
 	Settings.music = musicSlider.value
 	Settings.difficulty = difficultySlider.value
+	
+	if pauseMenu == null:
+		return
+	
+	if pauseMenu.is_paused == false:
+		visible = false
 
 func set_settings():
 	brightnessSlider.value = Settings.brightness
@@ -22,6 +36,11 @@ func set_settings():
 	difficultySlider.value = Settings.difficulty
 
 func go_back():
+	if IN_MAIN:
+		visible = false
+		pauseMenu.visible = true
+		return
+		
 	yield(get_tree().create_timer(0.1), "timeout")
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://MenusAndUI/Menus/StartMenu.tscn")
