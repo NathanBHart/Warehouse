e72bdc898891d66ce71ad806c6aa727a8d8812e1
Brady <thebradster7.bd@gmail.com>
2021-07-16 15:09:41 -0400

    Added light switches.

---
+++ World/LightSwitch.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+onready var playerDetector = $PlayerDetector
+
+var mouse_detected = false
+
+var MainInstances = ResourceLoader.MainInstances
+
+func _process(_delta):
+	playerDetector.cast_to = (MainInstances.Player.global_position + Vector2(0, -16)) - global_position
+	
+	if playerDetector.is_colliding(): return
+	
+	if not mouse_detected: return
+	
+	if Input.is_action_just_pressed("interact"):
+		MainInstances.CurrentRoom.flip_lights()
+
+func _on_LightSwitch_mouse_entered():
+	mouse_detected = true
+
+func _on_LightSwitch_mouse_exited():
+	mouse_detected = false
